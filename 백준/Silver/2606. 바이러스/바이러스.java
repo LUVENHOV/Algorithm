e7c49ec5e4d54ab710b2f03f68b68c3c5b675c88
LUVENOV <92037253+LUVENOV@users.noreply.github.com>
2023-10-20 11:14:39 +0900

    [Silver III] Title: 바이러스, Time: 84 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main{
-//BFS
-	static int N, M;
-	static int count = 0;
+public class Main {
+	
+	static int N, M,count;
 	static ArrayList<Integer>[] adjList;
 	static boolean[] visited;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		adjList = new ArrayList[N + 1];
-		visited = new boolean[N + 1];
-		for (int i = 1; i <= N; i++) {
-			adjList[i] = new ArrayList<>();
+	
+	static void dfs(int Node) {
+		//시작하는 노드 방문처리
+		visited[Node] = true;
+		//해당 Node에 해당하는 i들 중에서~
+		for(int i : adjList[Node]) {
+			//방문하지 않았다면~
+			if(!visited[i]) {
+				//방문처리하고
+				count++;
+				//연결된 거니까 count 세주고
+				dfs(i);
+				//해당 노드에 연결된 애 다시 dfs 탐색
+			}
 		}
-
-		for (int i = 0; i < M; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			adjList[a].add(b);
-			adjList[b].add(a);
-		}
-		
-		bfs(1);
-		System.out.println(count);
 		
 	}
 	
-	static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		visited = new boolean[N+1];
-		visited[start] = true;
-		queue.add(start);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		while(!queue.isEmpty()) {
-			int now = queue.poll();
-			for(int x : adjList[now]) {
-				if(visited[x] == false) {
-					visited[x] = true;
-					count++;
-					queue.add(x);
-				}
-			}
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		adjList = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		
+		for(int i = 1; i < N+1; i++) {
+			adjList[i] = new ArrayList<>();
 		}
+		
+		for(int i = 0; i < M ;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adjList[a].add(b);
+			adjList[b].add(a);
+		}
+		//1번 NODE 탐색
+		dfs(1);
+		
+		System.out.println(count);
+		
 	}
 	
 }
