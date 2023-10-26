1289e6bb6d5d1b5cb84b5136b40be53bc51acef1
LUVENOV <92037253+LUVENOV@users.noreply.github.com>
2023-10-26 17:19:18 +0900

    [Gold IV] Title: 연구소, Time: 668 ms, Memory: 295136 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -6,106 +6,102 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N,M,max;
-	static int[][] map;
-	static int[][] virusmap;
-	static int[] rr = {1,-1,0,0};
-	static int[] rc = {0,0,-1,1};
-	
-	static class Node{
+
+	static int N, M;
+	static int max = Integer.MIN_VALUE;
+	static int[][] board;
+	static int[][] virusboard;
+	static int[] rr = { -1, 1, 0, 0 };
+	static int[] rc = { 0, 0, -1, 1 };
+
+	static class Node {
 		int r, c;
 
 		public Node(int r, int c) {
 			this.r = r;
 			this.c = c;
 		}
-		
+
 	}
-	
+
+	static void DFS(int count) {
+		if (count == 3) {
+			BFS();
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (board[i][j] == 0) {
+					board[i][j] = 1;
+					DFS(count + 1);
+					board[i][j] = 0;
+				}
+			}
+		}
+
+	}// DFS
+
 	static void BFS() {
 		Queue<Node> que = new LinkedList<>();
-		//벽이 세워질 때마다 벽 초기화해야하므로 새로운 벽 복사
-		virusmap = new int[N][M];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				virusmap[i][j] = map[i][j];
-				if(virusmap[i][j] == 2) {
-					que.add(new Node(i,j));
+		// 벽을 세울 새로운 맵 생성
+		virusboard = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				virusboard[i][j] = board[i][j];
+				if (virusboard[i][j] == 2) {
+					que.add(new Node(i, j));
 				}
-				
 			}
 		}
-		
-		while(!que.isEmpty()) {
+
+		while (!que.isEmpty()) {
 			Node now = que.poll();
-			
-			for(int i = 0 ; i < 4; i++) {
+
+			for (int i = 0; i < 4; i++) {
 				int nr = now.r + rr[i];
 				int nc = now.c + rc[i];
-				
-				if (nr >= 0 && nr < N && nc >= 0 && nc < M && virusmap[nr][nc] == 0) {
-					virusmap[nr][nc] = 2;
-					que.add(new Node(nr,nc));
+
+				if (nr >= 0 && nc >= 0 && nr < N && nc < M && virusboard[nr][nc] == 0) {
+					virusboard[nr][nc] = 2;
+					que.add(new Node(nr, nc));
 				}
 			}
-			
+
 		}
-		//0인 곳 세기
+		// 0인 곳 세기
 		int count = 0;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(virusmap[i][j] == 0) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (virusboard[i][j] == 0) {
 					count++;
 				}
 			}
 		}
 		max = Math.max(max, count);
+
 	}
-	
-	static void DFS(int wallcount) {
-		if(wallcount == 3) {
-			//벽이 3개가 되면 바이러스 퍼뜨리기
-			BFS();
-			return;
-		}
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j< M; j++) {
-				if(map[i][j] == 0) {
-					//선택
-					map[i][j] = 1;
-					DFS(wallcount+1);
-					//원복
-					map[i][j] = 0;
-				}
-			}
-		}
-		
-	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
+
+		board = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		max = Integer.MIN_VALUE;
-		//전체 0중에서 3개 골라서 벽 세우기~ 맵 전체를 탐색하면서 
+
 		DFS(0);
-		
+
 		System.out.println(max);
-	}//main
-	
-	
+
+	}
+
 }
