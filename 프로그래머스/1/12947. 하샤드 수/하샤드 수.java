872087fbece4bca6dbc4ab5d1e0aea4933067417
LUVENHOV <92037253+LUVENHOV@users.noreply.github.com>
2023-11-28 21:45:10 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+class Solution {
+    public boolean solution(int x) throws IOException {
+        boolean answer = true;
+        
+        int sum = 0;
+        int num = x;
+        
+        while ( num >= 1){
+            sum += num % 10;
+            num /= 10;
+        }
+        
+        if(x % sum != 0) answer = false;
+        
+        return answer;
+        
+    }
+}
