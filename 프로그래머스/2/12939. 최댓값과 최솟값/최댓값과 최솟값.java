715cee5e58aa889b9d54436e8fb2a15e756beb54
LUVENHOV <92037253+LUVENHOV@users.noreply.github.com>
2024-01-02 21:15:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.53 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+        
+		String arr[] = s.split(" ");
+
+		for (int i = 0; i < arr.length; i++) {
+			if (min > Integer.parseInt(arr[i])) {
+				min = Integer.parseInt(arr[i]);
+			}
+			if (max < Integer.parseInt(arr[i])) {
+				max = Integer.parseInt(arr[i]);
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(min).append(" ").append(max);
+
+		return sb.toString();
+    }
+}
