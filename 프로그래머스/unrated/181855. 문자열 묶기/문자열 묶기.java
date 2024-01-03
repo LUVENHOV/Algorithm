c3c4b3f63f29275f421b9b43a51338f178e8c29f
LUVENHOV <92037253+LUVENHOV@users.noreply.github.com>
2024-01-03 22:43:56 +0900

    [level unrated] Title: 문자열 묶기, Time: 27.26 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+	public int solution(String[] strArr) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (String str : strArr) {
+			int length = str.length();
+			map.put(length, map.getOrDefault(length, 0) + 1);
+		}
+
+		int maxCount = 0;
+		for (int count : map.values()) {
+			maxCount = Math.max(maxCount, count);
+		}
+
+		return maxCount;
+	}
+}
