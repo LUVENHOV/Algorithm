1386178a9a7285af8778693a3729dfa313d424c0
LUVENHOV <92037253+LUVENHOV@users.noreply.github.com>
2024-07-19 23:36:10 +0900

    [Silver III] Title: 햄버거 분배, Time: 88 ms, Memory: 11860 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -1,45 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
- 
- 
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	public static int N, K;
-	public static char[] arr;
-	public static boolean[] visited;
-	public static int answer = 0;
+
+    public static int N, K;
+    public static char[] arr;
+    public static boolean[] visited;
+    public static int result = 0;
     public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	N = Integer.parseInt(st.nextToken());
-    	K = Integer.parseInt(st.nextToken());
-    	arr = new char[N];
-    	visited = new boolean[N];
-    	
-    	st = new StringTokenizer(br.readLine());
-    	arr = st.nextToken().toCharArray();
-    	
-//    	System.out.println(arr);
-    	for(int i=0;i<N;i++) {
-    		if(arr[i] == 'P') { //사람을 만났다면, K인 것 앞에서부터
-    			for(int j=-K;j<=K;j++) {
-    				if( i+j >= N) break;
-    				if( i+j < 0) continue;
-    				
-    				if(arr[i+j] == 'H' && visited[i+j] == false) {
-    					visited[i+j] = true;
-    					answer += 1;
-    					break;
-    				}
-    			}
-    		}
-    	}
-    	
-    	System.out.println(answer);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new char[N];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        arr = st.nextToken().toCharArray();
+
+        for(int i=0;i<N;i++) {
+            if(arr[i] == 'P') {
+                for(int j=-K;j<=K;j++) {
+                    if( i+j >= N) break;
+                    if( i+j < 0) continue;
+
+                    if(arr[i+j] == 'H' && visited[i+j] == false) {
+                        visited[i+j] = true;
+                        result += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
     }
- 
-    
+
+
 }
