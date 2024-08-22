f7412dd499e617050893668592730b3b211605bb
LUVENHOV <92037253+LUVENHOV@users.noreply.github.com>
2024-08-22 22:30:19 +0900

    [Silver IV] Title: 로프, Time: 244 ms, Memory: 25900 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+        int[] arr = new int[N];
+        int max = 0;
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++) {
+            arr[i] *= (N - i);
+            if(max < arr[i]) max = arr[i];
+        }
+        System.out.println(max);
+    }
+
+}
